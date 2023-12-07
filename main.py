c412e724afa9a86d604851b53aa76be0cef244c2
Abdallah Mohy <bebostudy@gmail.com>
2023-12-07 16:55:52 +0200

    new

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse, FileResponse
+from fastapi.staticfiles import StaticFiles
+import os
+
+# Specify the directory to save the uploaded files
+UPLOAD_DIR = "uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 app = FastAPI()
 
+# Serve static files (e.g., HTML) from the "static" directory
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/", response_class=HTMLResponse)
+async def get_upload_form():
+    return FileResponse("static/upload_form.html")
+
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile = File(...)):
-    return {"filename": file.filename}
+    file_path = os.path.join(UPLOAD_DIR, file.filename)
+    
+    # Save the file to the specified directory
+    with open(file_path, "wb") as buffer:
+        buffer.write(file.file.read())
+    
+    return {"filename": file.filename, "saved_path": file_path}
 
 
 # Set up CORS middleware
@@ -17,5 +37,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+
 
 
